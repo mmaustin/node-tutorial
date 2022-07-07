e5ac3a23825899c0a81e344df63514d89199c7d1
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-07-07 11:25:58 -0400

    practice with node events method

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Hello People!')
+const { log } = require('console');
+const EventEmitter = require('events');
+
+const CustomEmitter = new EventEmitter();
+
+CustomEmitter.on('response', (h, w)=>{
+    log(`data received some ${h}, so ${w} `);
+})
+
+CustomEmitter.emit('response', 'help', 'welp')
